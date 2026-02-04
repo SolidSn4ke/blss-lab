c6dc3bcc43f26d5539aeb4e5b6531db0ec40d260
Artemii Kuzmin <artsuper472@gmail.com>
2026-02-04 12:56:46 +0300

    init commit

---
+++ src/main/java/com/example/blsslab/BlssLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.blsslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlssLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlssLabApplication.class, args);
+	}
+
+}
