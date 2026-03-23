b55ba0aa0c41d3bdaeae96f024c9fbcdf9a615fa
SolidSn4ke <artsuper472@gmail.com>
2026-03-23 23:00:42 +0300

    add jwt service

---
+++ src/main/java/com/example/blsslab/service/JwtService.java
@@ -0,0 +1,52 @@
+package com.example.blsslab.service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.example.blsslab.model.dto.UserDTO;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+    @Value("${jwt.sign.key}")
+    private String signKey;
+
+    String generateToken(UserDTO user) {
+        Map<String, Object> claims = Map.of("role", user.getRole());
+        return Jwts.builder().setClaims(claims).setSubject(user.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(signKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
