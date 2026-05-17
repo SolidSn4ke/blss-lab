4890daee38f19a58bceebb5466f9cd350bfc69a7
SolidSn4ke <artsuper472@gmail.com>
2026-05-17 15:29:19 +0300

    remove hard coded erpnext connection info

--- erpnext-integration/src/main/java/com/example/blsslab/config/JcaConfig.java
+++ erpnext-integration/src/main/java/com/example/blsslab/config/JcaConfig.java
@@ -1,5 +1,6 @@
 package com.example.blsslab.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jca.support.LocalConnectionFactoryBean;
@@ -10,12 +11,24 @@ import jakarta.resource.spi.ManagedConnectionFactory;
 
 @Configuration
 public class JcaConfig {
+    @Value("${spring.erpnext.host}")
+    String host;
+
+    @Value("${spring.erpnext.port}")
+    Integer port;
+
+    @Value("${spring.erpnext.api.key}")
+    String apiKey;
+
+    @Value("${spring.erpnext.api.secret}")
+    String apiSecret;
+
     @Bean
     ManagedConnectionFactory managedConnectionFactory() {
         ErpNextManagedConnectionFactory connectionFactory = new ErpNextManagedConnectionFactory();
-        connectionFactory.setUrl("http://localhost:8080");
-        connectionFactory.setApiKey("aabe94341273b2d");
-        connectionFactory.setApiSecret("60e8ef383a39abb");
+        connectionFactory.setUrl(String.format("http://%s:%d", host, port));
+        connectionFactory.setApiKey(apiKey);
+        connectionFactory.setApiSecret(apiSecret);
         return connectionFactory;
     }
 
